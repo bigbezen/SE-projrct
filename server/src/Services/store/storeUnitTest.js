76a1cc96298f2e60bf6e875182f719c86538d614
bigbezen <matanbezen@gmail.com>
2016-12-11 18:00:49 +0200

    create unit tests for all services

---
+++ server/src/Services/store/storeUnitTest.js
@@ -0,0 +1,19 @@
+var assert = require('chai').assert;
+
+describe('store unit test', function () {
+
+    describe('test 01', function () {
+        it('pass test', function () {
+            //implementation of the test came here
+            assert.equal(1, 2, 'not pass');
+        });
+    });
+
+    describe('test 02', function () {
+        it('pass test', function () {
+            //implementation of the test came here
+            assert.equal(1, 2, 'not pass');
+        });
+    });
+
+});

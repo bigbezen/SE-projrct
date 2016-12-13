8cca7f8daf0f460684ff17ba627b34eb235a3441
bigbezen <matanbezen@gmail.com>
2016-12-13 12:44:31 +0200

    update tests structural

--- server/Tests/UnitTests/UnitTestsConroller.js
+++ server/Tests/UnitTests/UnitTestsConroller.js
@@ -1,12 +1,12 @@
-var userUnitTest = require( './user/userUnitTest.js');
-var storeUnitTest = require( './store/storeUnitTest.js');
-var shiftUnitTest = require( './shift/shiftUnitTest.js');
-var reportsUnitTest = require( './reports/reportsUnitTest.js');
-var recommendationUnitTest = require( './recommendations/recommendationUnitTest.js');
-var productUnitTest = require( './product/productUnitTest.js');
-var notificationsUnitTest = require( './notifications/notificationsUnitTest.js');
-var messagesUnitTest = require( './messages/messagesUnitTest.js');
-var encouragementsUnitTest = require( './encouragements/encouragementsUnitTest.js');
+var userUnitTest = require( './../../src/Services/user/userUnitTest.js');
+var storeUnitTest = require( './storeUnitTest.js');
+var shiftUnitTest = require( './shiftUnitTest.js');
+var reportsUnitTest = require( './reportsUnitTest.js');
+var recommendationUnitTest = require( './recommendationUnitTest.js');
+var productUnitTest = require( './productUnitTest.js');
+var notificationsUnitTest = require( './notificationsUnitTest.js');
+var messagesUnitTest = require( './messagesUnitTest.js');
+var encouragementsUnitTest = require( './encouragementsUnitTest.js');
 
 describe('grouped', function () {
     userUnitTest;

fef2a7194eb556ec83f04d158455bffa43d760c8
bigbezen <matanbezen@gmail.com>
2017-04-15 23:13:02 +0300

    adding excel formulas to the human resource report

--- server/main.js
+++ server/main.js
@@ -740,7 +740,7 @@ function _setapApiEndpoints() {
     });
 
     app.get('/manager/getSalaryForHumanResourceReport', async function (req, res) {
-        var result = await reportsService.getSalaryForHumanResourceReport(req.headers.sessionid ,req.headers.year,req.headers.month);
+        var result = await reportsService.getSalaryForHumanResourceReport('123456',2017,2);//req.headers.sessionid ,req.headers.year,req.headers.month);
         res.status(result.code).send(result.err);
     });
 

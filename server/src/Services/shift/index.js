1db52b9f743d39198f6c92eab859bc583befbe11
shahafstein <shahafstein@gmail.com>
2017-04-28 00:41:58 +0300

    new shift now sends the salesman a mail

--- server/src/Services/shift/index.js
+++ server/src/Services/shift/index.js
@@ -3,6 +3,8 @@ let us              = require('underscore');
 let logger          = require('../../Utils/Logger/logger');
 let dal             = require('../../DAL/dal');
 let permissions     = require('../permissions/index');
+let mailer          = require('../../Utils/Mailer/index');
+let moment          = require('moment');
 
 let shiftModel       = require('../../Models/shift');
 
@@ -49,8 +51,11 @@ let addShifts = async function(sessionId, shiftArr){
    for(let shift of shiftArr){
         let newShift = new shiftModel();
         newShift.storeId = shift.storeId;
-        if('salesmanId' in shift)
+        if('salesmanId' in shift) {
             newShift.salesmanId = shift.salesmanId;
+            let salesman = await dal.getUserByobjectId(shift.salesmanId);
+            sendMailOfShift(salesman, shift, storeDict[shift.storeId].name);
+        }
         newShift.storeId = shift.storeId;
         let updateStore = await dal.setStoreDefaultUser(shift.storeId, shift.salesmanId);
         newShift.startTime = shift.startTime;
@@ -74,6 +79,18 @@ let addShifts = async function(sessionId, shiftArr){
     });
 
     return {'code': 200, 'shiftArr': resultAddShift};
+};
+
+let sendMailOfShift = function(salesman, shift, storeName){
+    let date = moment(shift.startTime).format('DD-MM-YY');
+    let startHour = moment(shift.startTime).format('H:mm');
+    let endHour = moment(shift.endTime).format('H:mm');
+    let content = 'שובצה עבורך משמרת חדשה לתאריך: ' + date;
+    content += '\n\n' + 'בשעה: ' + startHour;
+    content += ' עד השעה: ' + endHour + '\n\n';
+    content += 'בחנות: ' + storeName;
+
+    mailer.sendMail([salesman.contact.email], 'IBBLS - שובצה עבורך משמרת חדשה', content);
 };
 
 let automateGenerateShifts = async function (sessionId, startTime, endTime){

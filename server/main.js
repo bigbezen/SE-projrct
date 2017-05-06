4bb4cc080d13a0c733b7f2bae5545caf0df0a186
bigbezen <matanbezen@gmail.com>
2017-05-06 10:08:18 +0300

    fix some issues in the validetor

--- server/main.js
+++ server/main.js
@@ -596,7 +596,7 @@ function _setapApiEndpoints() {
     });
 
     app.post('/management/addShifts', async function (req, res) {
-        if (!validator.addOrPublishShifts(req.body)) {
+        if (!validator.addShifts(req.body)) {
             res.status(404).send('invalid parameters');
             return;
         }
@@ -620,7 +620,7 @@ function _setapApiEndpoints() {
     });
 
     app.post('/management/publishShifts', async function (req, res) {
-        if (!validator.addOrPublishShifts(req.body)) {
+        if (!validator.publishShifts(req.body)) {
             res.status(404).send('invalid parameters');
             return;
         }

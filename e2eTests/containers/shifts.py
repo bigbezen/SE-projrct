b626b308a4bb9a898ab558ac8cd06c2a03471ad0
Aviram Adiri <aviramad@post.bgu.ac.il>
2017-06-24 15:43:47 +0300

    fixing ui tests

--- e2eTests/containers/shifts.py
+++ e2eTests/containers/shifts.py
@@ -3,6 +3,7 @@ publish = "//*[@id=\"app\"]/div/div[4]/div[1]/button[2]"
 edit = "//*[@id=\"app\"]/div/div[4]/div[2]/div/div[2]/div[2]/table/tbody/tr/td[10]/button"
 delete = "//*[@id=\"app\"]/div/div[4]/div[2]/div/div[2]/div[2]/table/tbody/tr/td[11]/button"
 
+
 store = "//*[@id=\"app\"]/div/div[4]/form/div[3]/select"
 username = "//*[@id=\"app\"]/div/div[4]/form/div[5]/select"
 type = "//*[@id=\"app\"]/div/div[4]/form/div[7]/select"
@@ -12,4 +13,4 @@ startTime = "//*[@id=\"app\"]/div/div[4]/form/div[12]/input"
 endTime = "//*[@id=\"app\"]/div/div[4]/form/div[14]/input"
 addBtn = "//*[@id=\"app\"]/div/div[4]/form/div[17]/button"
 
-publishBtn = "//*[@id=\"app\"]/div/div[4]/div[1]/button"
+publishBtn = "//*[@id=\"app\"]/div/div[4]/div[2]/div[1]/button"
